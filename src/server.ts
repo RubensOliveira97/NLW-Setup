9594e536fd6a3a3d508c2cd87888ee7623b3f864
Rubens Moreno <rubensmoreno27@gmail.com>
2023-01-17 22:48:46 -0300

    Setup geral do backend

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log('ooi')
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+
+const prisma = new PrismaClient()
+
+async function bootstrap() {
+  const app = Fastify({
+    logger: false,
+  })
+
+  app.register(cors);
+
+
+  app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits;
+  })
+  
+
+  await app.listen({ port: 3333 }).then(()=>{
+    console.log('Running')
+  })
+}
+
+bootstrap()
